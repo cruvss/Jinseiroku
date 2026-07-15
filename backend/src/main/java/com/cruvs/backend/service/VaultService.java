35c766cdb7a33fbe7cf25ec497731dde07eb10dc
Sachin Koirala <sachinkoirala9999@gmail.com>
2026-07-15 14:54:32 +0545

    remove unused import

--- backend/src/main/java/com/cruvs/backend/service/VaultService.java
+++ backend/src/main/java/com/cruvs/backend/service/VaultService.java
@@ -16,7 +16,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
